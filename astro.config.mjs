3b73612a6b2a937250b6c82ce04a417869e0e6fe
Adhiraj <adhirajd007@gmail.com>
2023-09-30 03:05:43 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://byt3h3ad.github.io',
+  // site: 'https://byt3h3ad.github.io',
+  site: 'http://adhiraj.me',
   // base: '/website',
   integrations: [tailwind()]
 });

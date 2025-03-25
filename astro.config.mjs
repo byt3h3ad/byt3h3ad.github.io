18de05b72073e16f76987d8ed4e7b5ce44c9fce7
byt3h3ad <adhirajd007@gmail.com>
2025-03-25 23:32:50 +0530

    yoloing at 2025-03-25T23.32

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,15 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 import { SITE } from "./src/consts";
+import { remarkModifiedTime } from "./src/lib/modified-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
   // site: 'https://byt3h3ad.github.io',
   site: SITE.URL,
   // base: '/website',
+  markdown: {
+    remarkPlugins: [remarkModifiedTime],
+  },
   integrations: [tailwind(), mdx(), sitemap()],
 });
